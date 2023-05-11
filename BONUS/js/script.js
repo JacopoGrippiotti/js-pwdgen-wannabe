8d0a525cb80c94eb899319113e67b3e8269fdfe9
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-05-11 20:25:00 +0200

    added bonus exercise

---
+++ BONUS/js/script.js
@@ -0,0 +1,85 @@
+let triangoloScelto = prompt("inserisci il nome del tuo triangolo scegliendo tra Scaleno, Equilatero e Isoscele");
+
+document.getElementById("tuoTriangolo").innerHTML = triangoloScelto;
+
+let areaTriangoloScelto;
+
+
+let lato;
+
+let base;
+
+let radiceDiTre;
+
+if (triangoloScelto = "isoscele"){
+
+   lato = prompt("definisci la lunghezza di uno qualunque dei lati del triangolo")
+
+}
+
+else if (triangoloScelto = "equilatero"){ 
+
+    lato = prompt("definisci la lunghezza di uno qualunque dei lati del triangolo")
+
+}
+
+else if (triangoloScelto = "scaleno") {
+   
+    base = prompt("inserisci la lunghezza della base del triangolo")
+   
+}
+
+let altezza
+
+if (triangoloScelto = "isoscele"){
+
+  altezza = prompt("inserisci l'altezza relativa al lato scelto")
+
+}
+
+else if (triangoloScelto = "scaleno"){
+ 
+  altezza = prompt("inserisci l'altezza relativa alla base del tuo triangolo")
+
+}
+
+
+if (triangoloScelto = "isoscele"){
+
+   areaTriangoloScelto = lato * altezza / 2
+
+}
+
+else if (triangoloScelto = "equilatero"){
+   
+   radiceDiTre = Math.sqrt(3)
+
+
+
+   areaTriangoloScelto = ((radiceDiTre / 4) * (lato ** 2))
+
+
+}
+
+
+
+else if (triangoloScelto = "scaleno"){
+
+areaTriangoloScelto = (base * altezza) / 2
+
+}
+
+
+
+
+
+
+
+
+document.getElementById("area").innerHTML = areaTriangoloScelto
+
+
+
+
+
+
