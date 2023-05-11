4c1c0609b75ed3a93b0785cb884d5831e9b52548
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-05-11 18:07:57 +0200

    added content to script.js file and to hmtl file. Completed exercise.

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let nome = prompt("inserisci il tuo nome")
+
+let cognome = prompt("inserisci il tuo cognome")
+
+let colorePreferito = prompt("inserisci il tuo colore preferito")
+
+document.getElementById("geppetto").innerHTML = nome + cognome + colorePreferito + "23"
